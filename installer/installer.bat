6cbcbf1c2319e82e21f684c2cee0db5c19145d0c
LandryLT <59315119+LandryLT@users.noreply.github.com>
2026-05-18 20:53:41 +0200

    better installer and styles added to git

--- installer/installer.bat
+++ installer/installer.bat
@@ -1,7 +1,13 @@
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
+git --version
+if errorlevel 1 goto errorNoGit
+
 git clone https://github.com/LandryLT/AnkiEZVocabAdder.git
 cd AnkiEZVocabAdder
 mkdir .\caches
 mkdir .\caches\jisho
+mkdir .\caches\neocities
 mkdir .\caches\audio
 mkdir .\caches\audio\words
 mkdir .\caches\audio\sentences
@@ -12,8 +18,24 @@ copy NUL vocab2add.txt
 call .\.venv\Scripts\activate
 pip install -r requirements.txt
 playwright install
-pause
-mklink /h ..\AnkiEZVocabAdder .\AnkiEZVocabAdder.bat
-mklink /h ..\vocab_to_add.txt .\vocab_to_add.txt
-del /S .\installer\*
 del ..\installer.bat
+echo .
+echo "All done..."
+echo .
+echo "Please see searchConfig.txt to configurate AnkiEZ"
+echo "Add search terms in vocab2add.txt (one per line)"
+echo "Then launch AnliEZVocabAdder.bat to get started !"
+echo .
+echo "Have fun, press Enter to continue"
+goto:eof
+
+:errorNoPython
+echo.
+echo Error^: Python not installed
+pause
+goto:eof
+
+:errorNoGit
+echo.
+echo Error^: Git not installed
+pause
