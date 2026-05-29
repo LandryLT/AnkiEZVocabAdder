c3ad5914b8eb052969643857f9cc7f9dff85ac47
LandryLT <59315119+LandryLT@users.noreply.github.com>
2026-05-29 14:14:06 +0200

    better updates

--- installer/installer.bat
+++ installer/installer.bat
@@ -15,6 +15,8 @@ mkdir .\caches\images
 mkdir .\caches\kanjis
 python -m venv .venv
 copy NUL vocab2add.txt
+copy .\installer\searchConfig_templ.txt .\searchConfig.txt
+copy .\installer\JLPsearchConfig_templ.txt .\scripts\scrappers\JLPTsearchConfig.txt
 call .\.venv\Scripts\activate
 pip install -r requirements.txt
 playwright install
